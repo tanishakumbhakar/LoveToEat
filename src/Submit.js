a5d57283c95437b261cb61f7e872da848a429f25
TANISHA KUMBHAKAR <46387444+tanishakumbhakar@users.noreply.github.com>
2020-05-29 08:52:39 +0530

    Added HTML form

--- src/Submit.js
+++ src/Submit.js
@@ -17,8 +17,36 @@ class Submit extends Component {
 	render() {
 		return (
 			<div>
-				<h2>Submit</h2>
-				<button onClick={this.submitRecipe}>Submit a Recipe</button>
+				
+				<div  classNameName="row">
+					<div classNameName="col-xs-12 col-sm-12">
+					<h2>Submit</h2>
+					<form>
+					  <div className="form-group">
+					    <label for="name">Name</label>
+					    <input type="text" className="form-control" id="name" placeholder="Enter the name of the recipe" />
+					    
+					  </div>
+					  <div className="form-group">
+					    <label for="description">Description</label>
+					    <textarea className="form-control" id="description" placeholder="Enter a brief description"/>
+					  </div>
+					   <div className="form-inline form-group">
+					    <label for="description">Quantity</label>
+					    <input type="text"  className="form-control" id="quantity" placeholder="Quantity"/>
+					  
+					  
+					    <label for="description">Ingredients</label>
+					    <input type="text"  className="form-control" id="ingredient" placeholder="Ingredients"/>
+					    <button onClick={this.submitRecipe} className="btn btn-info">Add</button>
+					  </div>
+					 
+					  
+					<button onClick={this.submitRecipe} className="btn btn-default">Submit a Recipe</button>
+					</form>
+					</div>
+				</div>
+				
 			</div>
 		);
 	}
